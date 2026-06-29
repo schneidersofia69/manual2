3028d01e9a58d0ec6d8040503bc94ca76acd9932
SofiaSch <schneidersofia69@gmail.com>
2026-06-29 14:34:02 -0300

    feat: agregar archivo js

---
+++ java.js
@@ -0,0 +1,7 @@
+console.log("manual");
+const consejos = [
+    "Buscar zona y herramientas para hacer un refugio",
+    "Buscar agua y potabilizarla",
+    "Encender un fuego seguro",
+    "Llevar herramientas utiles"
+];
